ca5d6e2a4aebed5debc26be16c3fb6147bfc096b
Drew Douglass <drewd@us.ibm.com>
2024-10-22 10:12:41 -0400

    HC settings

--- pyas2_rh/settings.py
+++ pyas2_rh/settings.py
@@ -78,11 +78,11 @@ import os
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USERNAME"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "NAME": "pyas2db",
+        "USER": "pyas2dbuser",
+        "PASSWORD": "pyas2dbuser",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
