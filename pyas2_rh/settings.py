098e60b455d6ee65f1795586f6b2d5c8348ab485
Drew Douglass <drewd@us.ibm.com>
2024-10-23 09:06:57 -0400

    update

--- pyas2_rh/settings.py
+++ pyas2_rh/settings.py
@@ -79,9 +79,9 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "pyas2db",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "pyas2-pyas2.itzroks-06000206uu-86xvsj-1615bdf0ff9ba5518cb8d7760c84e878-0000.us-south.containers.appdomain.cloud",
+        "USER": "pyas2dbuser",
+        "PASSWORD": "pyas2dbuser",
+        "HOST": "localhost",
         "PORT": "5432",
     }
 }
