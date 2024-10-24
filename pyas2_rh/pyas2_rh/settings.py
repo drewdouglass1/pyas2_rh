e46cefba8d841e3874db5c4d1eda5750af497f18
Drew Douglass <drewd@us.ibm.com>
2024-10-24 17:17:56 -0400

    static

--- pyas2_rh/pyas2_rh/settings.py
+++ pyas2_rh/pyas2_rh/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'#Location of static files
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
-STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
