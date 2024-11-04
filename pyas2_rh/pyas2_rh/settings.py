d8604febd889ef61667e01e13bc661543dc3d7a6
Drew Douglass <drewd@us.ibm.com>
2024-11-04 14:43:58 -0500

    debug

--- pyas2_rh/pyas2_rh/settings.py
+++ pyas2_rh/pyas2_rh/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-1&n&f4##g!a1aoc)^nt5#jg&x*hd6^5=2trtzyfkia5fn_lt2q"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
+DEBUG = True
 
-ALLOWED_HOSTS = ['.us-south.containers.appdomain.cloud']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
